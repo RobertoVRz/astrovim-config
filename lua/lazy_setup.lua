d9921c925a08a5e191808bb500ce8420fe1f04c6
RobertoVRz <roberto.vzrz@gmail.com>
2025-08-19 21:25:43 -0600

    Checkpoint Funcional Restructura

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -16,17 +16,18 @@ require("lazy").setup({
   event = 'BufEnter'
 },
 --{
- --   "Exafunction/windsurf.nvim",
- --   dependencies = {
- --       "nvim-lua/plenary.nvim",
- --       "hrsh7th/nvim-cmp",
- --   },
- --   config = function()
- --       require("codeium").setup({
- --       })
- --   end
+--    "Exafunction/windsurf.nvim",
+--    event = 'BufEnter',
+--    dependencies = {
+--        "nvim-lua/plenary.nvim",
+--        "hrsh7th/nvim-cmp",
+--    },
+--    config = function()
+--        require("codeium").setup({
+--        })
+--    end
 --},
-  
+  --
   -- [[
   -- {
   --    "supermaven-inc/supermaven-nvim",
